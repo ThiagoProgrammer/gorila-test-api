1df8d8b14af8e7dc7d675d66ecf745cdd2ee8a5f
Thiago Oliveira <thiagooliveira@Mac-mini-de-Thiago.local>
2021-12-09 17:46:46 -0300

    Fix: port

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { env } from 'process';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
